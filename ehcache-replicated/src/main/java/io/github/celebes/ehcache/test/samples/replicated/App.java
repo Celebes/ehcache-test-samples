754fa89b0cc740aaf958193c4d9f09760fbb3add
Celebeze <kris1337@gmail.com>
2015-03-15 14:51:18 +0100

    Dodano strukture projektow maven

---
+++ ehcache-replicated/src/main/java/io/github/celebes/ehcache/test/samples/replicated/App.java
@@ -0,0 +1,13 @@
+package io.github.celebes.ehcache.test.samples.replicated;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
